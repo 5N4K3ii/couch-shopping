990aa86a50fd25fd3a3099ad9d9d90ee29d94363
Nathan Kluender <n_kluender@yahoo.com>
2021-04-11 16:07:16 -0500

    Make a PWA

--- js/register-service-worker.js
+++ js/register-service-worker.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js', {
-        scope: '/'
+    navigator.serviceWorker.register('https://5n4k3ii.github.io/couch-shopping/service-worker.js', {
+        scope: 'https://5n4k3ii.github.io/couch-shopping/'
     }).then(function() {
         console.info('service worker registered');
     }).catch(function(e) {
--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,27 @@
 var CACHE_NAME = 'couch-shopping-list-v1';
 
 var resourcesToCache = [
-    '/',
-    '/edit-store.html',
-    '/shop.html',
-    '/css/style.css',
-    '/images/basket-cart.png',
-    '/images/delete.png',
-    '/images/pencil-png-200_200.png',
-    '/images/plus-sign.png',
-    '/images/clear-list.png',
-    '/js/ext/babel.min.js',
-    '/js/ext/pouchdb.min.js',
-    '/js/register-service-worker.js',
-    '/js/store.js',
-    '/js/app.js'
+    'https://5n4k3ii.github.io/couch-shopping/',
+    'https://5n4k3ii.github.io/couch-shopping/favicon.ico',
+    'https://5n4k3ii.github.io/couch-shopping/configure.html',
+    'https://5n4k3ii.github.io/couch-shopping/edit-store.html',
+    'https://5n4k3ii.github.io/couch-shopping/shop.html',
+    'https://5n4k3ii.github.io/couch-shopping/css/style.css',
+    'https://5n4k3ii.github.io/couch-shopping/images/appicon.png',
+    'https://5n4k3ii.github.io/couch-shopping/images/apple-touch-icon.png',
+    'https://5n4k3ii.github.io/couch-shopping/images/basket-cart.png',
+    'https://5n4k3ii.github.io/couch-shopping/images/clear-list.png',
+    'https://5n4k3ii.github.io/couch-shopping/images/delete.png',
+    'https://5n4k3ii.github.io/couch-shopping/images/home1600.png',
+    'https://5n4k3ii.github.io/couch-shopping/images/pencil-png-200_200.png',
+    'https://5n4k3ii.github.io/couch-shopping/images/plus-sign.png',
+    'https://5n4k3ii.github.io/couch-shopping/js/ext/babel.min.js',
+    'https://5n4k3ii.github.io/couch-shopping/js/ext/pouchdb.min.js',
+    'https://5n4k3ii.github.io/couch-shopping/js/aisle-app.js',
+    'https://5n4k3ii.github.io/couch-shopping/js/register-service-worker.js',
+    'https://5n4k3ii.github.io/couch-shopping/js/screenOrientation.js',
+    'https://5n4k3ii.github.io/couch-shopping/js/shop-app.js',
+    'https://5n4k3ii.github.io/couch-shopping/js/store.js'
 ];
 
 self.addEventListener('install', function(event) {
